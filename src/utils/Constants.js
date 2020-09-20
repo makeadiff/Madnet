0f1cc4f6093f5b25211d0030e8e970a9719d7367
Binny V A <binnyva@gmail.com>
2020-09-20 17:46:53 +0530

    Changed the API URL

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -6,7 +6,7 @@ let current_api_base_url = current_site_url + "api/"
 
 
 if(current_url.includes("localhost")) {
-    current_site_url = "http://localhost/makeadiff/"    
+    current_site_url = "http://localhost/MAD/"    
     current_api_base_url = current_site_url + "api/"
 
 } else if(current_url.includes("testing.makeadiff.in")) {
