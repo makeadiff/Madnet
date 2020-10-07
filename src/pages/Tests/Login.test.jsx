d629d5bee2188cad15d03879e2edbff527e16fdd
Binny V A <binnyva@gmail.com>
2020-10-07 14:14:00 +0530

    Cleanup

--- src/pages/Tests/Login.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Login from '../Login';
-
-test("Login page renders correctly", async () => {
-    const { findByText } = render(<Login />);
-    await findByText('Login to MADNet');
-});
---
+++ src/tests/jest/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../../App';
+var jest = require('jest');
+
+jest.mock('../../init-fcm.js', () => {
+    return {};
+});
+
+it('App Runs', () => {
+    shallow(<App />)
+});
