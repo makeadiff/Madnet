60cc852c5d34b30a547f1e787840eabbd680681b
Joel Louzado <jlouzado@gmail.com>
2021-01-28 21:33:22 +0530

    chore(prettier): fix changed files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jest": true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:protractor/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:protractor/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "protractor",
-         "prettier"
-    ],
-    "settings": {
-        "react": {
-            version: "detect"
-        }
-    },
-    "rules": {
-        "indent": ["warn", 4],
-        "react/prop-types": 0,                      // TODO: activate rule
-        "protractor/no-absolute-url": 0,            // TODO: activate rule
-        "protractor/no-describe-selectors": 0,      // TODO: activate rule
-        "protractor/no-repetitive-locators": 0,     // TODO: activate rule
-        "protractor/no-repetitive-selectors": 0,    // TODO: activate rule
-        "protractor/no-browser-sleep": 0,           // TODO: activate rule
-        "protractor/missing-wait-message": 0,       // TODO: activate rule
-    },
-};
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'protractor', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/prop-types': 0, // TODO: activate rule
+    'protractor/no-absolute-url': 0, // TODO: activate rule
+    'protractor/no-describe-selectors': 0, // TODO: activate rule
+    'protractor/no-repetitive-locators': 0, // TODO: activate rule
+    'protractor/no-repetitive-selectors': 0, // TODO: activate rule
+    'protractor/no-browser-sleep': 0, // TODO: activate rule
+    'protractor/missing-wait-message': 0 // TODO: activate rule
+  }
+}
