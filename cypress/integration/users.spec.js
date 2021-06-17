2379df29dfff64dc976dcaa6a8c8aa9de702bb5e
Binny V A <binnyva@gmail.com>
2021-06-17 14:16:23 +0530

    Merging Cypress branh into master - and giving it off to Bhanu to code

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -34,27 +34,5 @@ describe('Donation Page', () => {
     cy.contains(user_name)
   })
 
-//   it('Add new donation page should work', () => {
-//     cy.get("ion-fab-button").click();
-//     cy.url().should('include', "/donations/0");
-
-//     cy.get("ion-input#donor_name>input").type("Test Donor").should('have.value', "Test Donor");
-//     cy.get("ion-input#donor_email>input").type("donor.email@gmail.com").should('have.value',"donor.email@gmail.com");
-//     cy.get("ion-input#donor_phone>input").type("9886768565").should('have.value',"9886768565");
-//     cy.get("ion-input#amount>input").type("13").should('have.value',"13");
-
-//     // :TODO: Test differnt source, different dates, form validation
-
-//     cy.contains("Save").click();
-
-//     cy.get(".toast-message").should('include.text', "Donation details saved. Donation ID:");
-//     // Test if Tost goes away?
-//   });
-
-//   it('Shows added donation in listing page', () => {
-//     cy.contains("Donations").click();
-//     cy.url().should('include', "/donations");
-//     cy.contains("Test Donor") // There will be older donations from this donor. 
-//   })
 
 })
