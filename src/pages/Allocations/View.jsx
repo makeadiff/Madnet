a6113a276e3a27ca6fed3293d483c28fb0afcd5b
Binny V A <binnyva@gmail.com>
2021-10-30 15:21:00 +0530

    Fixed issue with allocations not dissappearing from view when deleted

--- src/pages/Allocations/View.jsx
+++ src/pages/Allocations/View.jsx
@@ -20,9 +20,6 @@ const TeacherView = () => {
   const { callApi, cache, unsetLocalCache } = React.useContext(dataContext)
   const [batches, setBatches] = React.useState([])
   const [shelter, setShelter] = React.useState('')
-  const [batch_id] = React.useState([])
-  const [level_id] = React.useState([])
-  const [teacher_id] = React.useState([])
   const { showMessage } = React.useContext(appContext)
 
   React.useEffect(() => {
@@ -65,13 +62,14 @@ const TeacherView = () => {
     cache[`teacher_view_${shelter_id}_${project_id}`]
   ])
 
-  const deleteMapping = (x) => {
+  const deleteMapping = (batch_id, level_id, teacher_id, batch_index, allocation_index) => {
     callApi({
-      url: `/batches/${batch_id[x]}/levels/${level_id[x]}/teachers/${teacher_id[x]}`,
+      url: `/batches/${batch_id}/levels/${level_id}/teachers/${teacher_id}`,
       method: 'delete'
     }).then(() => {
-      showMessage('Deleted')
+      batches[batch_index].allocations.splice(allocation_index, 1)
       unsetLocalCache(`teacher_view_${shelter_id}_${project_id}`)
+      showMessage('Deleted the teacher assignment')
     })
   }
 
@@ -90,21 +88,18 @@ const TeacherView = () => {
         </IonItem>
 
         <IonList>
-          {batches.map((batch, index) => {
+          {batches.map((batch, batch_index) => {
             if (batch.allocations.length > 0) {
               return (
-                <IonItem key={index}>
+                <IonItem key={batch_index}>
                   <div className="batch-area">
                     <IonLabel>
                       <h1>{batch.batch_name}</h1>
                     </IonLabel>
                     <IonList>
-                      {batch.allocations.map((alloc, ind) => {
-                        batch_id[ind] = batch.id
-                        teacher_id[ind] = alloc.user.id
-                        level_id[ind] = alloc.level.id
+                      {batch.allocations.map((alloc, allocation_index) => {
                         return (
-                          <IonItem key={ind} className="striped">
+                          <IonItem key={allocation_index} className="striped">
                             <IonLabel className="allocation-info">
                               <p>Teacher: {alloc.user.name}</p>
                               <p>Class Section: {alloc.level.level_name}</p>
@@ -116,7 +111,7 @@ const TeacherView = () => {
                             </IonLabel>
                             <IonButton
                               slot="end"
-                              onClick={() => deleteMapping(ind)}
+                              onClick={() => deleteMapping(batch.id, alloc.level.id, alloc.user.id, batch_index, allocation_index)}
                             >
                               Delete
                             </IonButton>
