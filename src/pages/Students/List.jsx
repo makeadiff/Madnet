77629ff0db315a5752359aac46811dd1643dc303
Vinayaka R Kamath <t-vkamath@microsoft.com>
2022-02-06 01:44:51 +0530

    Added Search, Sort and Filter ability for 'Students' Section
    - You can now run complex search on the Students tabs
    - Options to sort by ID and Name
    - The Students index is visible Shelterwise as well as
      at a city level.
    - Ran Prettier on the Student Pages

---
+++ src/pages/Students/List.jsx
@@ -0,0 +1,112 @@
+import {
+  IonLabel,
+  IonList,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonSelect,
+  IonSelectOption,
+  IonItem
+} from '@ionic/react'
+import { add } from 'ionicons/icons'
+import React from 'react'
+
+// import ChildSearch from './Search'
+import ChildDetail from '../../components/Child'
+import { authContext } from '../../contexts/AuthContext'
+import { dataContext } from '../../contexts/DataContext'
+import { appContext } from '../../contexts/AppContext'
+
+import './Form.css'
+import StudentSearch from './Search'
+
+const StudentList = ({ segment, shelter_id }) => {
+  const { user, hasPermission } = React.useContext(authContext)
+  const { callApi } = React.useContext(dataContext)
+  const [students, setStudents] = React.useState([])
+  const [city_id] = React.useState(user.city_id)
+  const { showMessage } = React.useContext(appContext)
+
+  React.useEffect(() => {
+    async function fetchChildList() {
+      let child_data = []
+      if (shelter_id) {
+        child_data = await callApi({
+          graphql: `{
+                    studentSearch(center_id: ${shelter_id}) { id name birthday sex center { name }}
+                    center(id: ${shelter_id}) { name }
+                  }`,
+          cache_key: `center_${shelter_id}_students`
+        })
+      } else {
+        child_data = await callApi({
+          graphql: `{
+                    studentSearch(city_id: ${city_id}) { id name sex birthday center { name }}
+                    city(id: ${city_id}) { name }
+                  }`,
+          cache_key: `city_${city_id}_students`
+        })
+      }
+      setStudents(child_data.studentSearch)
+    }
+    fetchChildList()
+  }, [city_id, shelter_id])
+
+  if (segment == 'all') {
+    return (
+      <IonContent className="dark">
+        <ListStudents students={students} sortByDefault="id" />
+        {hasPermission('kids_add') ? (
+          <IonFab vertical="bottom" horizontal="start" slot="fixed">
+            <IonFabButton routerLink={`/students/0`}>
+              <IonIcon icon={add} />
+            </IonFabButton>
+          </IonFab>
+        ) : null}
+      </IonContent>
+    )
+  } else if (segment == 'search') {
+    return <StudentSearch segment={segment} shelter_id={shelter_id} />
+  } else {
+    showMessage('Something went wrong while fetching student data', 'error')
+  }
+}
+
+const ListStudents = ({ students, sortByDefault }) => {
+  const [sortBy, setSortBy] = React.useState(sortByDefault)
+  if (students.length > 0) {
+    return (
+      <IonList>
+        <IonItem>
+          <IonLabel class="ion-text-right">Sort By</IonLabel>
+          <IonSelect
+            interface="popover"
+            value={sortBy}
+            placeholder="Choose"
+            onIonChange={(e) => setSortBy(e.target.value)}
+          >
+            <IonSelectOption value="id">ID</IonSelectOption>
+            <IonSelectOption value="name">Name</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        {students
+          .sort((item1, item2) => (item1[sortBy] > item2[sortBy] ? 1 : -1))
+          .map((child, index) => (
+            <ChildDetail key={index} child={child} index={index} />
+          ))}
+      </IonList>
+    )
+  } else {
+    return (
+      <IonList>
+        <IonItem>
+          <IonLabel>No Students found.</IonLabel>
+        </IonItem>
+      </IonList>
+    )
+  }
+}
+
+export default StudentList
+export { ListStudents }
---
+++ src/pages/Students/Search.jsx
@@ -0,0 +1,189 @@
+import {
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonCheckbox,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent
+} from '@ionic/react'
+import React from 'react'
+
+import { authContext } from '../../contexts/AuthContext'
+import { appContext } from '../../contexts/AppContext'
+import { dataContext } from '../../contexts/DataContext'
+import './Form.css'
+import { ListStudents } from './List'
+
+const StudentSearch = ({ segment, shelter_id }) => {
+  const { user } = React.useContext(authContext)
+  const { setLoading } = React.useContext(appContext)
+  const [shelter, setShelter] = React.useState([])
+  const [students, setStudents] = React.useState(null)
+  const [students_filtered, setStudentsFiltered] = React.useState([])
+  const { getUsers, callApi } = React.useContext(dataContext)
+  const [search, setSearch] = React.useState({
+    city_id: user.city_id,
+    id: '',
+    name: '',
+    shelter_ids: []
+  })
+
+  React.useEffect(() => {
+    async function fetchChildList() {
+      let child_data = []
+      if (shelter_id) {
+        child_data = await callApi({
+          graphql: `{
+                        studentSearch(center_id: ${shelter_id}) { id name birthday sex center { id name }}
+                        center(id: ${shelter_id}) { name }
+                      }`,
+          cache_key: `center_${shelter_id}_students`
+        })
+      } else {
+        child_data = await callApi({
+          graphql: `{
+                        studentSearch(city_id: ${search.city_id}) { id name sex birthday center { id name }}
+                        city(id: ${search.city_id}) { name }
+                      }`,
+          cache_key: `city_${search.city_id}_students`
+        })
+      }
+      setStudents(child_data.studentSearch)
+    }
+    fetchChildList()
+  }, [shelter_id])
+
+  //Fetching all shelters
+  React.useEffect(() => {
+    const fetchShelters = () => {
+      callApi({ url: `/cities/${search.city_id}/centers` }).then((data) => {
+        setShelter(data)
+      })
+    }
+    fetchShelters()
+  }, [segment])
+
+  const setSearchValue = (key, value) => {
+    const new_search = { ...search, [key]: value }
+    setSearch(new_search)
+  }
+
+  // This converts the selected checkboxes into an array of ids.
+  const setShelterSearch = (e) => {
+    let shelters = search.shelter_ids
+    const value = e.target.value
+    if (e.target.checked) {
+      shelters.push(value)
+    } else {
+      shelters = search.shelter_ids.filter((ele) => {
+        return ele !== value
+      })
+    }
+    setSearch({ ...search, shelter_ids: shelters })
+    console.log(shelters)
+  }
+
+  const searchStudents = async (e) => {
+    e.preventDefault()
+    if (
+      search.name === '' &&
+      search.id === '' &&
+      search.shelter_ids.length === 0
+    ) {
+      return // skip the search
+    }
+    setLoading(true)
+    let filtered_students = []
+    students.forEach((student) => {
+      if (
+        (search.name == '' ||
+          student.name.toLowerCase().includes(search.name.toLowerCase())) &&
+        (search.id == '' || student.id === search.id) &&
+        (search.shelter_ids.length == 0 ||
+          search.shelter_ids.includes(student.center.id))
+      ) {
+        filtered_students.push(student)
+      }
+      console.log(search.shelter_ids.includes(student.center.id))
+    })
+    console.log(filtered_students)
+    console.log(search.shelter_ids.length)
+    setStudentsFiltered(filtered_students)
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <form onSubmit={(e) => searchStudents(e)}>
+        <IonCard className="dark">
+          <IonCardHeader>
+            <IonCardTitle> Search Student(s)</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <InputRow
+              label="Student ID"
+              id="id"
+              type="text"
+              value={search.id}
+              onIonInput={(e) => setSearchValue('id', e.target.value)}
+            />
+            <InputRow
+              label="Student Name"
+              id="name"
+              type="text"
+              value={search.name}
+              onIonInput={(e) => setSearchValue('name', e.target.value)}
+            />
+            {shelter_id ? null : (
+              <div>
+                <IonItem>
+                  <IonLabel>Shelters</IonLabel>
+                </IonItem>
+                <div className="groups-area">
+                  {shelter.map((sh, index) => {
+                    return (
+                      <IonItem key={index} lines="none">
+                        <IonCheckbox
+                          value={sh.id}
+                          onIonChange={setShelterSearch}
+                        />
+                        <IonLabel> &nbsp; {sh.name}</IonLabel>
+                      </IonItem>
+                    )
+                  })}
+                </div>
+              </div>
+            )}
+
+            <IonItem>
+              <IonButton id="action" type="submit" size="default">
+                Search
+              </IonButton>
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
+      </form>
+      <ListStudents students={students_filtered} sortByDefault="id" />
+    </>
+  )
+}
+
+const InputRow = ({ id, label, type, value, onIonInput }) => {
+  return (
+    <IonItem>
+      <IonLabel position="stacked">{label}</IonLabel>
+      <IonInput
+        type={type}
+        id={id}
+        placeholder={label}
+        value={value}
+        onIonInput={onIonInput}
+      />
+    </IonItem>
+  )
+}
+
+export default StudentSearch
