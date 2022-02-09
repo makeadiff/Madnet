e7ef9f74aeb273787880cba675f49f1db10ae077
Rhythm-26 <rhythm.narula26@gmail.com>
2022-02-09 15:20:50 +0530

    ui updated

--- src/pages/Allocations/Index.jsx
+++ src/pages/Allocations/Index.jsx
@@ -1,7 +1,15 @@
-import { IonPage, IonList, IonItem, IonLabel, IonContent, IonCardContent, IonInput} from '@ionic/react'
+import {
+  IonPage,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonContent,
+  IonCardContent,
+  IonInput,
+  IonCard
+} from '@ionic/react'
 import React from 'react'
 
-import './Index.css'
 import { authContext } from '../../contexts/AuthContext'
 import { dataContext } from '../../contexts/DataContext'
 import { appContext } from '../../contexts/AppContext'
@@ -16,12 +24,11 @@ const TeacherIndex = () => {
   const [teachers, setTeachers] = React.useState([])
   const [city_id] = React.useState(user.city_id)
   const [location, setLocation] = React.useState('')
-  const [query, setQuery] = React.useState("")
+  const [query, setQuery] = React.useState('')
 
   React.useEffect(() => {
     async function fetchTeacherList() {
-      const user_data = await callApi({ url: 'cities/' + city_id + '/users' }) // 1
-      // const user_data = await callApi({url:"cities/" + city_id + "/teachers"})  // 2 (Diff values. Why?)
+      const user_data = await callApi({ url: 'cities/' + city_id + '/users' })
       const city_name = await callApi({ url: 'cities/' + city_id })
 
       if (user_data) {
@@ -41,33 +48,41 @@ const TeacherIndex = () => {
         back={`/shelters/${shelter_id}/projects/${project_id}/batch/${batch_id}/level/${level_id}/view-teachers`}
       />
       <IonContent className="dark">
-        <IonList>
+        <IonCard>
           <IonCardContent>
-                <IonItem>
-                  <IonInput 
-                    type="text"
-                    id="name"
-                    placeholder="Enter Volunteer Name"
-                    onIonChange={(e) => setQuery(e.target.value)}
-                    class="placeholder-text"
-                  />
-                </IonItem>
+            <IonList>
+              <IonItem>
+                <IonInput
+                  type="text"
+                  id="name"
+                  placeholder="Filter by Name"
+                  onIonChange={(e) => setQuery(e.target.value)}
+                  class="placeholder-text"
+                />
+              </IonItem>
+              {teachers
+                .filter((teacher) => {
+                  if (query === '') {
+                    return teacher
+                  } else if (
+                    teacher.name.toLowerCase().includes(query.toLowerCase())
+                  ) {
+                    return teacher
+                  }
+                })
+                .map((teacher, index) => {
+                  return (
+                    <IonItem
+                      key={index}
+                      routerLink={`/shelters/${shelter_id}/projects/${project_id}/batch/${batch_id}/level/${level_id}/assign-teachers/${teacher.id}`}
+                    >
+                      <IonLabel>{teacher.name}</IonLabel>
+                    </IonItem>
+                  )
+                })}
+            </IonList>
           </IonCardContent>
-            {teachers.filter(teacher => {
-                if (query === '') {
-                  return teacher;
-                } else if (teacher.name.toLowerCase().includes(query.toLowerCase())) {
-                  return teacher;
-                }
-              }).map((teacher, index) => {
-                return (
-                  <IonItem key={index}
-                    routerLink={`/shelters/${shelter_id}/projects/${project_id}/batch/${batch_id}/level/${level_id}/assign-teachers/${teacher.id}`}>
-                    <IonLabel>{teacher.name}</IonLabel>
-                  </IonItem>
-                )
-            })}
-        </IonList>
+        </IonCard>
       </IonContent>
     </IonPage>
   )
